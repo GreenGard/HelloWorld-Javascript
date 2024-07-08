08b1e69b71711d815685503faa776ff24bbddb7c
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-07-08 17:01:40 +0200

    variable exercises

---
+++ basicKnowledge.js
@@ -0,0 +1,53 @@
+// declare variable 
+let num;
+
+// assign 5 to num
+num = 5;
+
+
+console.log(num);
+
+//declare and and assign multiple variables 
+
+let num2 = 5, num3 = 6, num4 = 7;
+
+console.log(num2, num3, num4);
+
+// assign hello to greeting
+let greeting = "hello"; 
+console.log(greeting); 
+
+// change the value of greeting welcome
+greeting = "welcome"; 
+console.log(greeting); 
+
+// valid
+let message = "hello";
+let _message = "hello";
+let $message = "hello";
+
+//case sensitive and don´t use keywords
+let age = 23;
+let Age = 20;
+
+console.log(age); 
+console.log(Age);
+
+// use const for immutable variable must be initialized when declared
+const number = 5;
+
+
+// integer value
+let integer_number = -5;
+console.log(integer_number);
+
+// floating-point value
+let float_number = 3.17;
+console.log(float_number);
+
+//boolean
+let dataChecked = true;
+console.log(dataChecked);  // true
+
+let valueCounted = false;
+console.log(valueCounted);  // false
