6c9df814b167a8699cd11d0717e5dc2f37672297
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-07-08 19:55:34 +0200

    added som comparing code

--- basicKnowledge.js
+++ basicKnowledge.js
@@ -51,3 +51,77 @@ console.log(dataChecked);  // true
 
 let valueCounted = false;
 console.log(valueCounted);  // false
+
+
+let x = 5;
+
+// addition operator
+console.log("Addition: x + 3 = ", x + 3);
+
+// subtraction operator
+console.log("Subtraction: x - 3 =", x - 3);
+
+// multiplication operator
+console.log("Multiplication: x * 3 =", x * 3);
+
+// division operator
+console.log("Division: x / 3 =", x / 3);
+
+// remainder operator
+console.log("Remainder: x % 3 =", x % 3);
+
+// increment operator
+console.log("Increment: ++x =", ++x);
+
+// decrement operator
+console.log("Decrement: --x =", --x);
+
+// exponentiation operator
+console.log("Exponentiation: x ** 3 =", x ** 3);
+
+// assignment operator
+let a = 7;
+console.log("Assignment: a = 7, a =", a);
+
+// addition assignment operator
+a += 5;  // a = a + 5
+console.log("Addition Assignment: a += 5, a =", a);
+
+// subtraction assignment operator
+a -= 5;  // a = a - 5
+console.log("Subtraction Assignment: a -= 5, a =", a);
+
+// multiplication assignment operator
+a *= 2;  // a = a * 2
+console.log("Multiplication Assignment: a *= 2, a =", a);
+
+// division assignment operator
+a /= 2;  // a = a / 2
+console.log("Division Assignment: a /= 2, a =", a);
+
+// remainder assignment operator
+a %= 2;  // a = a % 2
+console.log("Remainder Assignment: a %= 2, a =", a);
+
+// exponentiation assignment operator
+a **= 2;  // a = a**2
+console.log("Exponentiation Assignment: a **= 7, a =", a);
+
+
+const testa = 3, testat = 2;
+console.log(testa > testat);
+
+// Output: true 
+
+
+const xx = 5, yy = 3;
+console.log((xx < 6) && (yy < 5));
+
+// Output: true
+
+
+
+named = "Per";
+
+// display name on the console
+console.log("Hello " + named);
